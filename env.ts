dc1958498f6a64ee880cfe9a4f54e86eb39ddde6
Mainak Das <mainakdas104@gmail.com>
2026-06-29 12:29:53 +0530

    Add initial project setup with Prisma, Clerk, and UI components
    - Added .gitignore to exclude generated Prisma files.
    - Created components.json for UI configuration.
    - Introduced env.ts for environment variable management.
    - Updated next.config.ts to include environment configuration.
    - Added dependencies for Prisma, Clerk, and UI components in package.json.
    - Created initial Prisma schema and seed files.
    - Implemented basic authentication routes for sign-in and sign-up.
    - Added a button component with styling and variants.
    - Updated global CSS for improved theming and layout.
    - Established utility functions for class name management.

---
+++ env.ts
@@ -0,0 +1,32 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import * as z from "zod";
+
+export const env = createEnv({
+  server: {
+    CLERK_SECRET_KEY: z.string(),
+    DATABASE_URL: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: z.string(),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  runtimeEnv: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL:
+      process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL:
+      process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  // experimental__runtimeEnv: {
+  //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  // }
+});
