13eb9fa8abf79f525824261810335787a9b2d215
Mainak Das <mainakdas104@gmail.com>
2026-07-09 16:52:33 +0530

    Implement date selection and note display in JournalData component
    - Added a Calendar component for selecting dates, allowing users to filter journal entries by date.
    - Enhanced the JournalData component to display notes associated with the selected date, improving user interaction and data visibility.
    - Updated the page component to prefetch journal data based on the selected date, ensuring accurate content rendering.

--- app/(main-app)/journal/[journalId]/journal-data.tsx
+++ app/(main-app)/journal/[journalId]/journal-data.tsx
@@ -1,12 +1,49 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Card, CardContent } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { orpc } from "@/lib/orpc.query";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { notFound } from "next/navigation";
+import { format, parseISO } from "date-fns";
+import { Plus } from "lucide-react";
+import {
+  notFound,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import { Fragment, useState } from "react";
 
 export const JournalData = ({ id }: { id: string }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const initialDate = searchParams.get("date")
+    ? parseISO(searchParams.get("date") ?? format(new Date(), "yyyy-MM-dd"))
+    : new Date();
+
+  const [date, setDate] = useState(initialDate);
+
+  const handleDateChange = (selected: Date | undefined) => {
+    if (!selected) return;
+
+    setDate(selected);
+
+    const params = new URLSearchParams(searchParams);
+
+    params.set("date", format(selected, "yyyy-MM-dd"));
+
+    router.replace(`${pathname}?${params.toString()}`);
+  };
+
   const { data, isError } = useSuspenseQuery(
-    orpc.journalRouter.getJournalById.queryOptions({ input: { id } }),
+    orpc.journalRouter.getJournalById.queryOptions({
+      input: { id, date: format(date, "yyyy-MM-dd") },
+    }),
   );
 
   if (!data) return notFound();
@@ -15,5 +52,41 @@ export const JournalData = ({ id }: { id: string }) => {
     return <p>Something went wrong</p>;
   }
 
-  return <div>{data.title}</div>;
+  return (
+    <div>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-12">
+        <Card className="md:col-span-4">
+          <CardContent>
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={handleDateChange}
+              fixedWeeks
+              className="p-0 [--cell-size:--spacing(9.5)] w-full"
+            />
+          </CardContent>
+        </Card>
+        <ScrollArea className="md:col-span-8 rounded-md border">
+          <div className="p-4">
+            <div className="flex items-center justify-between">
+              <h4 className="mb-4 text-sm leading-none font-medium">Notes</h4>
+              <Button variant={"secondary"}>
+                <Plus />
+              </Button>
+            </div>
+            {data.notes.length === 0 ? (
+              <div className="text-sm text-muted-foreground">No notes</div>
+            ) : (
+              data.notes.map((item) => (
+                <Fragment key={item.id}>
+                  <div className="text-sm">{item.title}</div>
+                  <Separator className="my-2" />
+                </Fragment>
+              ))
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  );
 };
--- app/(main-app)/journal/[journalId]/page.tsx
+++ app/(main-app)/journal/[journalId]/page.tsx
@@ -2,17 +2,24 @@ import { HydrateClient } from "@/components/hydration";
 import { orpc } from "@/lib/orpc.query";
 import { getQueryClient } from "@/lib/query/get-query-client";
 import { JournalData } from "./journal-data";
+import { format } from "date-fns";
 
 export default async function JournalIdPage({
   params,
+  searchParams,
 }: PageProps<"/journal/[journalId]">) {
   const { journalId } = await params;
+  const { date } = (await searchParams) as {
+    date: string;
+  };
+
+  const selectedDate = date ?? format(new Date(), "yyyy-MM-dd");
 
   const queryClient = getQueryClient();
 
   queryClient.prefetchQuery(
     orpc.journalRouter.getJournalById.queryOptions({
-      input: { id: journalId },
+      input: { id: journalId, date: selectedDate },
     }),
   );
 
