9ac178e6e220490d62b9c090abb014dcba2f1fbf
Mainak Das <mainakdas104@gmail.com>
2026-07-13 13:01:13 +0530

    Implement loading state for trash component with table structure and skeletons
    - Replaced the simple loading message with a structured table layout to enhance user experience during data loading.
    - Integrated skeleton components for visual placeholders while data is being fetched, improving perceived performance.
    - Added a button for future menu interactions, maintaining a consistent UI design.

--- app/(main-app)/trash/loading.tsx
+++ app/(main-app)/trash/loading.tsx
@@ -1,3 +1,44 @@
+import { MoreHorizontalIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-  return <div>loading....</div>;
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Notes</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead />
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <TableRow key={index}>
+            <TableCell>
+              <Skeleton className="h-8 w-8" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-8 w-48" />
+            </TableCell>
+            <TableCell>
+              <Button variant="ghost" size="icon" className="size-8">
+                <MoreHorizontalIcon />
+                <span className="sr-only">Open menu</span>
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
