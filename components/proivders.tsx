f9125868f796efe2254248bbfcd281b2c042c9a5
Mainak Das <mainakdas104@gmail.com>
2026-07-13 13:30:39 +0530

    Remove unused React import in Providers component

--- components/proivders.tsx
+++ components/proivders.tsx
@@ -3,7 +3,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { getQueryClient } from "@/lib/query/get-query-client";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryClientProvider } from "@tanstack/react-query";
-import type * as React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
